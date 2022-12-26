c071002bb5c346177dadc74a12eb175b08ad3630
Vishwanath Shinde <vishwanathshinde988@gmail.com>
2022-12-27 00:45:54 +0530

    first

---
+++ Slip_9/Q22.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+#include <conio.h>
+
+class Time
+{
+    int hr, min, sec;
+
+public:
+    void settime(int x, int y, int z)
+    {
+        hr = x;
+        min = y;
+        sec = z;
+    }
+
+    void showtime()
+    {
+        cout << "\n"
+             << hr << ":" << min << ":" << sec << endl;
+    }
+
+    Time add(Time t)
+    {
+        Time t1;
+        t1.sec = sec + t.sec;
+        t1.min = t1.sec / 60;
+        t1.sec = t1.sec % 60;
+        t1.min = t1.min + min + t.min;
+
+        t1.hr = t1.min / 60;
+        t1.min = t1.min % 60;
+        t1.hr = t1.hr + hr + t.hr;
+        return t1;
+    }
+};
+
+int main()
+{
+    int h, m, s;
+    cout << "\n Enter hr:\t";
+    cin >> h;
+    cout << "\n Enter min:\t";
+    cin >> m;
+    cout << "\n Enter sec:\t";
+    cin >> s;
+    Time t1;
+    t1.settime(h, m, s);
+    t1.showtime();
+    Time t2;
+    cout << "\n Enter hr:\t";
+    cin >> h;
+    cout << "\n Enter min:\t";
+    cin >> m;
+    cout << "\n Enter sec:\t";
+    cin >> s;
+
+    t2.settime(h, m, s);
+    t2.showtime();
+
+    Time t3;
+    cout << "\t Addition of 2 times : ";
+    t3 = t1.add(t2);
+    t3.showtime();
+    return 0;
+}
