8f7df24e344217aadaa7702688cc010776add211
Vishwanath Shinde <vishwanathshinde988@gmail.com>
2023-01-02 00:28:26 +0530

    first

--- Practice.cpp
+++ Practice.cpp
@@ -1,39 +1,51 @@
 #include<iostream>
 using namespace std;
 
-class student
+class array
 {
     public:
-        int maximum(int,int);
-        int maximum(int*, int arraylength);
+        int n, i, j;
+        float a[10], temp;
+
+        void accept()
+        {
+            cout << "Enter how many elements: ";
+            cin >> n;
+            for(i = 0; i<n; i++)
+            {
+                cout << "Enter elements: ";
+                cin >> a[i];
+            }
+            cout << "Array is: ";
+
+            for(i=0; i<n; i++)
+            {
+                cout << a[i] << "\t"; 
+            }
+
+            //sorting
+            for(i=0; i<n; i++)
+            {
+                for(j=0; j<n-i-1; j++)
+                {
+                    if(a[j] > a[j+1])
+                    {
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+            //sorted
+            cout << "Sorted array is: ";
+            for(i=0; i<n; i++)
+            {
+                cout << a[i] << "\t";
+            }
+        }
 };
-
-int student :: maximum(int a, int b)
-{
-    if(a>b)
-        cout << a << "is maximum";
-    else
-        cout << b << "is maximum";
-}
-
-int student :: maximum(int arr[] , int n)
-{
-    int i;
-    if(arr[0] < arr[i])
-    {
-        arr[0] = arr[i];
-    }
-    cout << "\nLargest element: " << arr[0];
-}
-
 int main()
 {
-    student s;
-    s.maximum(10,20);
-
-    int arr[] = {1,2,3,4,5,6,7,8,9,10};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    cout << "\nLargest in given array is " << ( arr , n ) ;
-    s.maximum(arr,5);
-    return 0;
+    array ar;
+    ar.accept();
 }
